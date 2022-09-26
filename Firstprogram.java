d5ebf09a4fafb2fc783962e8a9003b32620337f2
Jayy1950 <114063180+Jayy1950@users.noreply.github.com>
2022-09-26 14:43:23 +0530

    Add files via upload

---
+++ Firstprogram.java
@@ -0,0 +1,20 @@
+public class Firstprogram
+{
+    static String FirstName ="Jay";
+    static String LastName="Yadav";
+    static int age=22;
+    static {
+        System.out.println(FirstName);
+        System.out.println(LastName);
+        System.out.println(age);
+    }
+    public static void main(String[] args){
+
+    }
+
+
+    
+}
+
+
+
